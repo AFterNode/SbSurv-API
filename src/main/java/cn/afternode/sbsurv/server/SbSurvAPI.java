7ffe0fb32de441dd5905e992df4144d8986d9ab4
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-11-07 21:47:47 +0800

    Add common library

--- src/main/java/cn/afternode/sbsurv/server/SbSurvAPI.java
+++ src/main/java/cn/afternode/sbsurv/server/SbSurvAPI.java
@@ -1,5 +1,8 @@
 package cn.afternode.sbsurv.server;
 
+import cn.afternode.sbsurv.common.protocol.MessageType;
+import cn.afternode.sbsurv.common.protocol.ProtocolAdapter;
+import cn.afternode.sbsurv.common.protocol.messages.*;
 import cn.afternode.sbsurv.server.listener.ListenerManager;
 import cn.afternode.sbsurv.server.listener.PayloadListener;
 import cn.afternode.sbsurv.server.managers.PlayerManager;
@@ -10,6 +13,8 @@ import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidParameterException;
 
 public class SbSurvAPI {
     private final PlayerManager pm;
@@ -33,5 +38,21 @@ public class SbSurvAPI {
     public void crashClient(Player player) throws InvocationTargetException {
         SPacketCustomPayload pkt = PacketBuilder.buildCrasher();
         prm.sendServerPacket(player, pkt.getHandle());
+    }
+
+    public Message readMessage(byte[] data) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        String msg = new String(data, StandardCharsets.UTF_8);
+        MessageType type = ProtocolAdapter.getType(msg);
+        if (type == MessageType.Request) {
+            return ProtocolAdapter.fromString(msg, RequestMessage.class);
+        } else if (type == MessageType.Response) {
+            return ProtocolAdapter.fromString(msg, ResponseMessage.class);
+        }  else if (type == MessageType.Order) {
+            return ProtocolAdapter.fromString(msg, OrderMessage.class);
+        } else if (type == MessageType.Query) {
+            return ProtocolAdapter.fromString(msg, QueryMessage.class);
+        } else {
+            throw new InvalidParameterException();
+        }
     }
 }
